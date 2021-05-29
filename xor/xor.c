e6b20a47df0816957b4613ca49581f47d7340f3e
rosyne99 <laridain.rosi@gmail.com>
2021-05-29 21:04:14 +0400

    Programme 3

---
+++ xor/xor.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+
+{
+
+  char str_message[500], ch;
+
+  int j, key;
+
+  printf("Enter a sentence that you want to encrypt: ");
+
+  gets(str_message);
+
+  printf("Enter the key: ");
+
+  scanf("%d", &key);
+
+  for(j = 0; str_message[j] != '\0'; ++j){
+
+    ch = str_message[j];
+
+    if(ch >= 'a' && ch <= 'z'){
+
+      ch = ch + key;
+
+      if(ch > 'z'){
+
+        ch = ch - 'z' + 'a' - 1;
+
+      }
+
+      str_message[j] = ch;
+
+    }
+
+    else if(ch >= 'A' && ch <= 'Z'){
+
+      ch = ch + key;
+
+      if(ch > 'Z'){
+
+        ch = ch - 'Z' + 'A' - 1;
+
+      }
+
+      str_message[j] = ch;
+
+    }
+
+  }
+
+  printf("Encrypted message: %s", str_message);
+
+
+  return 0;
+  system("pause");
+
+}
